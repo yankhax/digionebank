eeaf95adaa8c7e6fd70f0b241db4a898a803070f
ianque <charlesphellipedev@gmail.com>
2021-03-06 22:46:33 -0300

    classe pessoa

---
+++ Pessoa.kt
@@ -0,0 +1,11 @@
+class Pessoa{
+    var nome: String = "Charles"
+    var cpf: String = "111.555.454-55"
+}
+
+fun main(){
+    val pessoa = Pessoa()
+
+    println(pessoa.nome)
+    println(pessoa.cpf)
+}
