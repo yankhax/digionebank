815f564531bb667f94281a4885d7958337feb102
ianque <charlesphellipedev@gmail.com>
2021-03-09 23:59:46 -0300

    iniciamos o estudo sobre abstração

--- Pessoa.kt
+++ Pessoa.kt
@@ -1,27 +1,7 @@
 // Estrutura //
-class Pessoa{
-    var nome:String = "Charles"
 
-    var cpf:String = "111.555.454-55"
-    private set //modificador de visibildiade deixa a propriedade privada de ser alterada, mas pode ser obtida (get)
-
-    // Classe Interna //
-    inner class End {
-        var rua:String = "Rua José Alves"
-    }
-
-    //construtor secundario
-    constructor()
-
-    fun NomeCPF() = "$nome $cpf"
-
-}
-
-fun main(){
-    val pessoa = Pessoa()
-
-    println(pessoa.nome)
-    println(pessoa.cpf)
-
-    println(pessoa.End().rua)
-}
+//classe abstrata não tem instanciamento
+abstract class Pessoa(
+   val nome: String,
+   val cpf: String
+)
